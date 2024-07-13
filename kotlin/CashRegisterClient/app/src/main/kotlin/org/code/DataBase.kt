10397a5f7d966b83305dda0a0f93f79133254192
LiranBratt2121 <brattlirannin@gmail.com>
2024-07-13 20:57:29 +0300

    Add database wrapper

---
+++ kotlin/CashRegisterClient/app/src/main/kotlin/org/code/DataBase.kt
@@ -0,0 +1,43 @@
+package org.code
+
+import com.google.auth.oauth2.GoogleCredentials
+import com.google.cloud.firestore.Firestore
+import com.google.firebase.FirebaseApp
+import com.google.firebase.FirebaseOptions
+import com.google.firebase.cloud.FirestoreClient
+import java.io.FileInputStream
+import java.io.InputStream
+import org.code.ContentData
+
+class DataBase(secretFilePath: String) {
+    private val m_serviceAccount: InputStream
+    private val m_credentials: GoogleCredentials
+    private val m_database: Firestore
+
+    init {
+        m_serviceAccount = FileInputStream(secretFilePath)
+        m_credentials = GoogleCredentials.fromStream(m_serviceAccount)
+
+        val options = FirebaseOptions.builder().setCredentials(m_credentials).build()
+
+        FirebaseApp.initializeApp(options)
+        m_database = FirestoreClient.getFirestore()
+    }
+
+    fun getDB(): Firestore {
+        return m_database
+    }
+
+    fun saveToDb(clientId: String, content: String): Boolean {
+        val docRef = m_database.collection(clientId).document()
+        val contentData = ContentData(content)
+
+        return try {
+            docRef.set(contentData).get()
+            true
+        } catch (e: Exception) {
+            e.printStackTrace()
+            false
+        }
+    }
+}
