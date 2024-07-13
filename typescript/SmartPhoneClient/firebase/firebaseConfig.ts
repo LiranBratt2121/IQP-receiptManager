eb87bc263e3be0a98e0b088e55abfd78cfc0720b
LiranBratt2121 <brattlirannin@gmail.com>
2024-07-14 01:37:32 +0300

    Update name

--- typescript/SmartPhoneClient/firebase/firebaseConfig.ts
+++ typescript/SmartPhoneClient/firebase/firebaseConfig.ts
@@ -2,7 +2,7 @@
 import { FirebaseApp, initializeApp } from "firebase/app";
 import { Firestore, getFirestore } from "firebase/firestore";
 
-import firebaseConfig from "./secret_firebaseConfig"
+import firebaseConfig from "./secretFirebaseConfig"
 
 const app: FirebaseApp = initializeApp(firebaseConfig);
 const db: Firestore = getFirestore(app);
---
+++ typescript/SmartPhoneClient/firebase/secretFirebaseConfig.ts
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDVadFb_9iQd9b35YUcKjx_KSka9ilfxgw",
+    authDomain: "receipt-reader-a8efb.firebaseapp.com",
+    projectId: "receipt-reader-a8efb",
+    storageBucket: "receipt-reader-a8efb.appspot.com",
+    messagingSenderId: "321378062651",
+    appId: "1:321378062651:web:7f9a373d48d750ea76a71e",
+    measurementId: "G-5DR63RR18P"
+};
+
+export default firebaseConfig;
