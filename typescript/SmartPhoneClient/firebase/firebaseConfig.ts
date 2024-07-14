dd6e7e0ff6466d4d7bc4508954e97a500508fa3b
LiranBratt2121 <brattlirannin@gmail.com>
2024-07-14 22:23:59 +0300

    Add auth to firebase config

--- typescript/SmartPhoneClient/firebase/firebaseConfig.ts
+++ typescript/SmartPhoneClient/firebase/firebaseConfig.ts
@@ -1,10 +1,11 @@
 // firebaseConfig.ts
 import { FirebaseApp, initializeApp } from "firebase/app";
 import { Firestore, getFirestore } from "firebase/firestore";
-
+import { Auth, getAuth } from "firebase/auth"
 import firebaseConfig from "./secretFirebaseConfig"
 
 const app: FirebaseApp = initializeApp(firebaseConfig);
+const auth: Auth = getAuth();
 const db: Firestore = getFirestore(app);
 
-export { db };
+export { db, auth };
